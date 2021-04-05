3bc079b4507f2c1e23e522b727f11e484d0091a7
Ben Gianni <bgianni576@gmail.com>
2021-04-05 09:49:26 -0400

    Add project files.

---
+++ dotnet/Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        /*
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dotnet_db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        */
+        public Context(DbContextOptions<Context> options)
+    : base(options)
+        {
+        }
+    }
+}
---
+++ dotnet/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        [Required]
+        public String FirstName { get; set; }
+        [Required]
+        public String LastName { get; set; }
+
+    }
+}
